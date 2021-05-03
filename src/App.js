6db467b7f4304d57ab9ace8d3f52ab507faa383d
ReneSena <rene.sena3005@gmail.com>
2021-05-02 22:02:28 -0300

    feat(global styles): added styles generic on app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { Routes } from './routes';
 import { store } from './store';
 
+import './global/global.css';
+
 function App() {
 	return (
 		<Provider store={store}>
