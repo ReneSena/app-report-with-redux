9e069506d4db479d1dbcb9ad8708d0631af06a4a
ReneSena <rene.sena3005@gmail.com>
2021-05-04 05:03:42 -0300

    feat(pages/report): created flow of loading table data

---
+++ src/pages/report/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { InputSearch } from '../../components/Forms/InputSearch';
+import { Layout } from '../../components/Layout';
+import api from '../../data/data_full.json';
+import { loaderReport } from '../../store/modules/report/actions';
+
+import './style.css';
+import { TableVirtualized } from './TableVirtualized';
+
+export function ReportPage() {
+	const dispatch = useDispatch();
+	const { list } = useSelector(state => state.report);
+	const [search, setSearch] = React.useState('');
+
+	React.useEffect(() => {
+		dispatch(loaderReport(api.data));
+	}, [dispatch]);
+
+	React.useEffect(() => {
+		if (search.length === 0) dispatch(loaderReport(api.data));
+	}, [search]);
+
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		const dataSearch = list.filter(
+			item => item.product === search || item.origin === search
+		);
+
+		const isSearchValid =
+			search.length === 0
+				? dispatch(loaderReport(api.data))
+				: dispatch(loaderReport(dataSearch));
+
+		return isSearchValid;
+	}
+
+	return (
+		<Layout>
+			<section className="section__filter">
+				<InputSearch
+					id="filter"
+					name="filter"
+					placeholder="Searching for Product or Origin"
+					value={search}
+					handlerChange={event => setSearch(event.target.value)}
+					handleSubmit={event => handleSubmit(event)}
+				/>
+			</section>
+
+			<section className="section__data">
+				<TableVirtualized
+					listLength={list.length}
+					listElements={list}
+				/>
+			</section>
+		</Layout>
+	);
+}
